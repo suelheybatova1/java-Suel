261f8d77de4354bd1880f1ddfafe52984cf17e9e
suelheybatova1 <123533417+suelheybatova1@users.noreply.github.com>
2025-06-19 22:27:40 +0400

    types of variables

---
+++ urokiJava_project/src/urokijava/DataTypes.java
@@ -0,0 +1,32 @@
+package urokijava;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        //celiye chisla
+        byte my_byte = -120;
+        short my_short = 10000;
+        int my_int = 33000;
+        long my_long = 10000000;
+        //desatichniye cisla
+        double my_double = 1000.556;
+        float my_float = 1000.556f;
+        //chat
+        char my_char = '&';
+        //boolean
+        boolean my_bool = true;
+        //string
+        String my_string = "Privet,Aueshniki";
+//printiem
+        System.out.println(my_byte);
+        System.out.println(my_short);
+        System.out.println(my_int);
+        System.out.println(my_long);
+        System.out.println(my_double);
+        System.out.println(my_float);
+        System.out.println(my_char);
+        System.out.println(my_bool);
+        System.out.println(my_string);
+
+    }
+
+}
