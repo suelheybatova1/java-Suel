fe9b7efa380c2e6566669bfa714e3d393d997307
suelheybatova1 <123533417+suelheybatova1@users.noreply.github.com>
2025-06-19 22:54:18 +0400

    ArithmeticOperators task

---
+++ urokiJava_project/src/urokijava/ArithmeticOperators.java
@@ -0,0 +1,26 @@
+package urokijava;
+
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+
+        int x = 20;
+        int y = 5;
+
+        int sum = x+y;
+        int sub = x - y;
+        int mult = x * y;
+        int div = x / y;
+
+        System.out.println( sum );
+        System.out.println( sub );
+        System.out.println( mult );
+        System.out.println( div );
+        System.out.println(x % y);
+        System.out.println( sum*2);
+        System.out.println( sub/4);
+        System.out.println( div*1);
+        System.out.println( mult -100);
+
+
+    }
+}
