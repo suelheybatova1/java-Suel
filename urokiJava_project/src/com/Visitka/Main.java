215f4173f14ebe576adae6a62d47268c1793bebf
suelheybatova1 <123533417+suelheybatova1@users.noreply.github.com>
2025-06-20 12:54:15 +0400

    homewotk1

---
+++ urokiJava_project/src/com/Visitka/Main.java
@@ -0,0 +1,37 @@
+package com.Visitka;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //main info about the person
+        String name = "Aflatun";
+        int age = 55;
+        float height = 1.79f;
+        char fav_char = '%';
+        boolean fact = false;
+        double weight = 89.900;
+
+        //operations that we do
+        System.out.println(age*36);
+        System.out.println(weight/(height*height));
+
+        //usage of increment
+         age++;
+        System.out.println(age);
+
+        //usage of
+        if ( age > 18) {
+            System.out.println("You can drink alcohol and sleep with people");
+        }
+            else {
+            System.out.println("Sit evde, and help ur father");
+        }
+            if (name.equals("Alesha") && age >= 20) {
+                System.out.println("Gj");
+            }
+                else{
+                System.out.println("Unlucky");
+                }
+            }
+
+    }
