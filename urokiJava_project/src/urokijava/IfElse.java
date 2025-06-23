39d133fe56ab1372224f5b48cc89396b021769ca
suelheybatova1 <123533417+suelheybatova1@users.noreply.github.com>
2025-06-23 23:34:23 +0400

    if else and switch

---
+++ urokiJava_project/src/urokijava/IfElse.java
@@ -0,0 +1,21 @@
+package urokijava;
+
+public class IfElse {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        if (a == b) {
+            System.out.println(a / b);
+        }
+            else if (a < b){
+            System.out.println(a*b);
+        }
+            else if (a!=b) {
+            System.out.println("a is lox");
+        }
+            else{
+            System.out.println("idk da brat");
+        }
+    }
+}
--- urokiJava_project/src/urokijava/TaskMassive.java
+++ urokiJava_project/src/urokijava/TaskMassive.java
@@ -9,17 +9,15 @@ public class TaskMassive {
 
         //tut ya sdelala fignyu s tem, chtobi vivodilos ima i cislo
         System.out.println("resultatik budet : ");*/
-        
-        String[][] data = {
-                {"Ali", "Alihandro", "Maryam", "Aflatun", "Elemdar"},
-                {"44", "45", "66", "75", "33"}
-        };
 
-        System.out.println(data[0][0] + data[1][0]); // Ali44
-        System.out.println(data[0][1] + data[1][1]); // Alihandro45
-        System.out.println(data[0][2] + data[1][2]); // Maryam66
-        System.out.println(data[0][3] + data[1][3]); // Aflatun75
-        System.out.println(data[0][4] + data[1][4]); // Elemdar33
+
+        String[] names = {"Ali","Alihandro","Maryam","Aflatun","Elemdar"};
+        int[] numbers = {44,45,66,75,33};
+
+        System.out.println(names[0] + numbers[0] + "," + names[1] + numbers[1] + "," +names[2] + numbers[2] + ","+ names[3] + numbers[3] + ","+ names[4] + numbers[4] + ",");
+
+
+
     }
 }
 
