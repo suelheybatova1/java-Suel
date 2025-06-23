11f075e5764f72b5d133f76cc2ba70fb5212100e
suelheybatova1 <123533417+suelheybatova1@users.noreply.github.com>
2025-06-24 00:06:20 +0400

    updated massives with for

--- urokiJava_project/src/urokijava/TaskMassive.java
+++ urokiJava_project/src/urokijava/TaskMassive.java
@@ -4,20 +4,31 @@ public class TaskMassive {
     public static void main(String[] args) {
 
         //eto u nas dva otdelnix massiva
-        /*String[] names = {"Ali","Alihandro","Maryam","Aflatun","Elemdar"};
-        int[] numbers = {44,45,66,75,33};
-
+        //tring[][] data = {
+             //   {"Ali ", "Alihandro ", "Maryam ", "Aflatun ", "Elemdar "},
+              //  {"44", "45", "66", "75", "33"}
+        //};
         //tut ya sdelala fignyu s tem, chtobi vivodilos ima i cislo
-        System.out.println("resultatik budet : ");*/
+        //System.out.println("resultatik budet :");
+        //for (int i = 0; i < 5; i++) {
+           // System.out.println(data[0][i] + data[1][i]);
+       // }
+        String[] names = {"Ali", "Alihandro", "Maryam", "Aflatun", "Elemdar"};
+        int[] numbers = {44, 45, 66, 75, 33};
 
-
-        String[] names = {"Ali","Alihandro","Maryam","Aflatun","Elemdar"};
-        int[] numbers = {44,45,66,75,33};
-
-        System.out.println(names[0] + numbers[0] + "," + names[1] + numbers[1] + "," +names[2] + numbers[2] + ","+ names[3] + numbers[3] + ","+ names[4] + numbers[4] + ",");
-
-
-
+        System.out.println("naw result budet: ");
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i] + numbers[i]);
+        }
     }
 }
+
+
+
+
+        //String[] names = {"Ali","Alihandro","Maryam","Aflatun","Elemdar"};
+       // int[] numbers = {44,45,66,75,33};
+
+       // System.out.println(names[0] + numbers[0] + "," + names[1] + numbers[1] + "," +names[2] + numbers[2] + ","+ names[3] + numbers[3] + ","+ names[4] + numbers[4] + ",");
+//}}
 
